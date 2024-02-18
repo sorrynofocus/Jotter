f55bface628f4301ed56c52993f88dfce199dd68
SorryNoFocus <thechriswinters@gmail.com>
2024-02-18 02:03:56 -0700

    Initial commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections.ObjectModel;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace Jotter
+{
+    public partial class MainWindow : Window
+    {
+        public class Note
+        {
+            public string? Title { get; set; }
+            public string? Text { get; set; }
+        }
+
+        public ObservableCollection<Note> Notes { get; set; }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            Notes = new ObservableCollection<Note>();
+            DataContext = this;
+        }
+
+        private void AddNote_Click(object sender, RoutedEventArgs e)
+        {
+            Notes.Add(new Note { Title = "New Note", Text = "Enter your text here." });
+        }
+
+        private void DeleteNote_Click(object sender, RoutedEventArgs e)
+        {
+            if (MyNotesListView.SelectedItem != null)
+                Notes.Remove((Note)MyNotesListView.SelectedItem);
+        }
+
+        private void DeleteNoteFromContextMenu_Click(object sender, RoutedEventArgs e)
+        {
+            if (MyNotesListView.SelectedItem != null)
+                Notes.Remove((Note)MyNotesListView.SelectedItem);
+        }
+
+        private void Note_DoubleClick(object sender, RoutedEventArgs e)
+        {
+            if (sender is ListViewItem item && item.DataContext is Note note)
+                MessageBox.Show($"Double-clicked on: {note.Title}\n\nText: {note.Text}");
+        }
+
+        private void OpenNote_Click(object sender, RoutedEventArgs e)
+        {
+            // Implement logic for opening a note
+        }
+
+        private void ExitApp_Click(object sender, RoutedEventArgs e)
+        {
+            // Implement logic for saving settings/notes
+            Environment.Exit(0);
+        }
+
+        //Coee behind for dragging window since windowstyle = none
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+                this.DragMove();
+        }
+
+
+
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # Jotter
-A quick note taking application
+A quick note taking application for those who store useful data you need to get at a later time. 
+
+Jotter is an application *currently under development*. This isn't even alpha yet, more prototype as I am learning WPF. The functionality is very limited at the moment. 
+
+The idea:
+
+- Clean look
+- Very easy to use
+- easy to use back-end database or user friendly data.
+- aimed at adding notes somewhere that can be easily brought back up when needed.
+- ability to _export_ the notes to a text file, markdown, html, etc. 
+- a settings area to customize the application and its  functionality.
+
+
+
+
